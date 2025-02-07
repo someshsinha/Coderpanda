8bbf9958725ccb39193bb6c8abb40856cc032aba
someshsinha <175540924+someshsinha@users.noreply.github.com>
2025-02-07 11:27:09 +0530

    Add files via upload

--- day3.py
+++ day3.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-st.title("hi everyone I am fine")
-st.write("Python requires practise")
-data=pd.DataFrame({
-    'c1':[10,20,30,40],
-    'c2':['A','B','C','D']})
-st.write("Below is the table for data")
-st.write(data)                
+
+st.title('Welcome to Python website')
+st.write('This is a simple website to show how to use Python with Streamlit')
+
+data=pd.DataFrame({'c1':[1,2,3,4,5],'c2':["A","B",30,40,50]})
+st.write(data)
+chart_data = np.random.randn(20, 3)
+st.bar_chart(chart_data)
